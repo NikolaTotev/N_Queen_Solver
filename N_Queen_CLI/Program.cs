e990d113435d79852383d9aeb84ecc36385442dc
Nikola <nikola.r.totev@gmail.com>
2021-10-31 21:01:23 +0200

    Created CLI project

---
+++ N_Queen_CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace N_Queen_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
